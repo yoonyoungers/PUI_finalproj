f45a6ea7199be0a36bcef52277d2d7b44f4d44d3
Yoon Young <yoonyouk@andrew.cmu.edu>
2019-12-10 17:44:29 -0500

    changes

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,4 @@
-
-
-
-
+//JS for fading in
 $(function(){  // $(document).ready shorthand
   $('.monster').fadeIn('slow');
 });
@@ -29,3 +26,24 @@ $(document).ready(function() {
     });
     
 });
+
+
+
+//slide show JS 
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
